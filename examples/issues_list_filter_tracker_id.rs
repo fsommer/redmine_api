982ade3fcc1cc0d348bc53ca17fe0d95b07aaa56
Florian Sommer <fsommer1986@gmail.com>
2017-08-27 21:11:02 +0200

    Add tracker_id filter example for issues.list

---
+++ examples/issues_list_filter_tracker_id.rs
@@ -0,0 +1,16 @@
+extern crate redmine_api;
+
+use redmine_api::RedmineApi;
+
+fn main() {
+    let redmine = RedmineApi::new(
+        "http://localhost:8080".to_string(),
+        "bbde69d1999dde8f497199f49bb7b577389b6c0e".to_string(),
+    );
+
+    let result = redmine.issues.filter()
+        .with_tracker_id(2)
+        .list();
+
+    println!("Result: {:?}", result);
+}
